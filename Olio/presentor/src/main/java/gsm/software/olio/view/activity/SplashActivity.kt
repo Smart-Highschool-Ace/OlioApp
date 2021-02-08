27bc3bc8c0268545bb9912fd6f1a641d07a2ae7f
structsize <52183019+structsize@users.noreply.github.com>
2021-02-08 23:30:44 +0900

    [CREATE] create splash activity

---
+++ Olio/presentor/src/main/java/gsm/software/olio/view/activity/SplashActivity.kt
@@ -0,0 +1,12 @@
+package gsm.software.olio.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.olio.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
