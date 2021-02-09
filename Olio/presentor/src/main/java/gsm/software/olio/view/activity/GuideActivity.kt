1e735b55b3dd68850dbb58ffb84a670a8c3ce900
structsize <52183019+structsize@users.noreply.github.com>
2021-02-09 10:19:00 +0900

    [CREATE] create guide

---
+++ Olio/presentor/src/main/java/gsm/software/olio/view/activity/GuideActivity.kt
@@ -0,0 +1,12 @@
+package gsm.software.olio.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.olio.R
+
+class GuideActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guide)
+    }
+}
