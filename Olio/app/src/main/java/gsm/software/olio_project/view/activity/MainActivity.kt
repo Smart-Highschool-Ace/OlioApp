3fa4d8faf2803eb0da617290a513574187b7da65
structsize <52183019+structsize@users.noreply.github.com>
2021-02-15 23:30:23 +0900

    [UPDATE] repackage app

---
+++ Olio/app/src/main/java/gsm/software/olio_project/view/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package gsm.software.olio_project.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import gsm.software.olio_project.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
