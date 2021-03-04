b88f71aa892b949055aaeb52489a05d7c3342f80
structsize <52183019+structsize@users.noreply.github.com>
2021-03-04 20:31:34 +0900

    [UPDATE] update mainActivity

--- presenter/src/main/java/org/software/meister/gsm/olio/base/BaseFragment.kt
+++ presenter/src/main/java/org/software/meister/gsm/olio/base/BaseFragment.kt
@@ -1,4 +1,4 @@
-package com.example.knowledgeoverflow.base
+package org.software.meister.gsm.olio.base
 
 import android.os.Bundle
 import android.view.LayoutInflater
--- presenter/src/main/java/org/software/meister/gsm/olio/base/BaseViewModel.kt
+++ presenter/src/main/java/org/software/meister/gsm/olio/base/BaseViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.knowledgeoverflow.base
+package org.software.meister.gsm.olio.base
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
